5603b1ef6482e7583299a2814ac5593bfbad36f1
MaHdixvi <131790640+MaHdixvi@users.noreply.github.com>
2025-08-10 18:07:20 +0330

    change base url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // core/environments/environment.ts
 export const environment = {
   production: false, // Indicates whether the app is running in production mode
-  apiUrl: 'http://localhost:3000/api', // Base URL for the API
+  apiUrl: 'http://localhost:5037/api', // Base URL for the API
   loggingLevel: 'debug', // Logging level (e.g., debug, info, warn, error)
   enableAnalytics: false, // Enable analytics tracking
   appVersion: '1.0.0', // Application version
