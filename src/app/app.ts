a062ea49e2047b7edc6dd0c64ca29497829998cb
MaHdixvi <131790640+MaHdixvi@users.noreply.github.com>
2025-08-10 13:43:06 +0330

    implementing app section

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,33 @@
 // app.component.ts
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { Sidebar, Footer, Header } from "./shared/components";
+import { filter } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Sidebar, Footer,CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('vamino_frontend');
+  currentRoute = '';
+
+  constructor(private router: Router) {
+    // تشخیص مسیر فعلی
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.currentRoute = event.url;
+      });
+  }
+
+  // بررسی اینکه آیا مسیر فعلی یک صفحه احراز هویت است
+  isAuthRoute(): boolean {
+    return (
+      this.currentRoute.includes('auth/login') ||
+      this.currentRoute.includes('auth/register')
+    );
+  }
 }
